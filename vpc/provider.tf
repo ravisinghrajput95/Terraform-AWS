089a5bfc37fb0dafc5db38a871b24810ef7b3c63
ravisinghrajput95 <ravisinghrajput005@gmail.com>
2022-07-27 23:37:50 +0530

    vpc

---
+++ vpc/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">=2.7.0"
+    }
+  }
+
+}
+
+provider "aws" {
+  profile = var.profile
+  region  = var.region
+
+}
